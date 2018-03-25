d0e86a0f3f7fbf8210babea364445baffd37956b
Kevin Solorio <soloriok@gmail.com>
2018-03-25 11:53:41 -0700

    add migration to avoid `new` in the tests

--- test/fundraiser.js
+++ test/fundraiser.js
@@ -2,7 +2,7 @@ const Fundraiser = artifacts.require('./Fundraiser.sol');
 
 contract('Fundraiser', () => {
   it('deploys', async () => {
-    const instance = await Fundraiser.new();
+    const instance = await Fundraiser.deployed();
     assert(instance);
   });
 });
