46c23ea9fc580c5517fec965fd2e8896e86f2639
Kevin Solorio <soloriok@gmail.com>
2018-04-22 22:05:24 -0700

    update network for local deployment

--- migrations/2_deploy_fundraiser.js
+++ migrations/2_deploy_fundraiser.js
@@ -1,5 +1,5 @@
 const Fundraiser = artifacts.require('./Fundraiser.sol');
 
-module.exports = function(deployer, network, accounts) {
+module.exports = function(deployer) {
   deployer.deploy(Fundraiser);
 }
