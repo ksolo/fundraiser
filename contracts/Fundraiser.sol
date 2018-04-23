a2ca91c8cfba2f90db89f7ea4dece77c6d234e41
Kevin Solorio <soloriok@gmail.com>
2018-04-22 21:51:38 -0700

    updated to use _ naming for params

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -13,7 +13,7 @@ contract Fundraiser is Ownable {
   // the value at a later time
   struct Donation {
     uint256 time;
-    uint256 exchange;
+    uint256 exchangeRate;
     uint256 value;
   }
 
@@ -27,26 +27,26 @@ contract Fundraiser is Ownable {
   Donation[] public donations;
 
   // notifies the client that the donation has been completed
-  event DonationReceived();
+  event DonationReceived(address indexed _donor, uint256 _time, uint256 _value, uint _exchangeRate);
 
   // update the organization's address
-  function setOrganization(address orgAddress) public onlyOwner {
-    organization = orgAddress;
+  function setOrganization(address _organization) public onlyOwner {
+    organization = _organization;
   }
 
   // transfer the funds to the organization, create a new
   // donation record, and emit the DonationReceived event
-  function donate(uint256 exchangeRate) public payable {
+  function donate(uint256 _exchangeRate) public payable {
     organization.transfer(address(this).balance);
 
-    Donation memory donation = Donation({value: msg.value, exchange: exchangeRate, time: now});
+    Donation memory donation = Donation({value: msg.value, exchangeRate: _exchangeRate, time: now});
     donationIndexes[msg.sender].push(donationsCount);
     donationsCount = donations.push(donation); // push returns the new length
 
-    emit DonationReceived();
+    emit DonationReceived(msg.sender, donation.time, donation.value, donation.exchangeRate);
   }
 
-  function donationIndexesForDonor(address donor) public view returns(uint256[]) {
-    return donationIndexes[donor];
+  function donationIndexesForDonor(address _donor) public view returns(uint256[]) {
+    return donationIndexes[_donor];
   }
 }
